d385b51bfefe3f5fa3e6c1dc77b2a14a8f10ffac
VeryThankYou <erikbuur@hotmail.com>
2025-04-30 09:17:43 +0200

    Gridlines

--- 12Histogram.py
+++ 12Histogram.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 means = np.load("AllMeans.npy")
 hist, bin_edges = np.histogram(means, bins=20)
+plt.grid()
 plt.stairs(hist, bin_edges, fill=True)
 plt.xlabel("Mean temperature in C")
 plt.ylabel("Number of buildings")
